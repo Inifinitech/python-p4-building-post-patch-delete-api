a54a0aa4759505e866e18bbf53f454ba19dd76da
Kush <kuriaisac@gmail.com>
2024-10-07 13:07:48 +0300

    updates changes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -67,6 +67,75 @@ def reviews():
 
     return response
 
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id==id).first()
+
+    if review == None:
+        error = {"message": "This record does not exist in our database. Please try again."}
+        response = make_response(jsonify(error))
+        
+        return response
+    else:
+        if request.method == 'GET':
+            review_dict = review.to_dict()
+
+            response = make_response(review_dict, 200)
+
+            return response
+    
+        elif request.method == 'PATCH':
+            for attr in request.form:
+                setattr(review, attr, request.form.get(attr))
+
+            db.session.add(review)
+            db.session.commit()
+
+            review_dict = review.to_dict()
+
+            response = make_response(review_dict, 200)
+
+            return response
+        
+        elif request.method == 'DELETE':
+            db.session.delete(review)
+            db.session.commit()
+
+            response_dict = {
+                "delete_successful": True,
+                "message": "Review deleted."
+            }
+
+            response = make_response(response_dict, 200)
+
+            return response
+
+@app.route('/reviews', methods=['GET', 'POST'])
+def reviews():
+    data = request.get_json()
+
+    if request.method == 'GET':
+        reviews = [review.to_dict() for review in Review.query.all()]
+
+        response = make_response(reviews, 200)
+        return response
+    elif request.method == 'POST':
+        new_review = Review(
+            score = data('score'),
+            comment = data('comment'),
+            game_id = data['game_id'],
+            user_id = data['user_id'],
+        )
+        db.session.add(new_review)
+        db.session.commit()
+
+        reponse_dict = new_review.to_dict()
+
+        response = make_response(reponse_dict, 200)
+
+        return response
+    
+        
 @app.route('/users')
 def users():
 
